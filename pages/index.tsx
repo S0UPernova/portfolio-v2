ad042a1e1a6ed7b383ab6cb42bbfde7b81c4f52e
Michael Kielpinski <kielpinski.michael@gmail.com>
2022-12-14 16:40:32 -0800

    worked on projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ import styles from '../styles/Home.module.scss'
 import TypedText from "../components/typedText"
 // import { useIsVisible } from "../hooks/useIsVisible"
 // import { useRef } from 'react'
-
 export default function Home() {
   // const ref = useRef();
   // const isVisible = useIsVisible(ref);
@@ -25,9 +24,9 @@ export default function Home() {
             <iframe loading="lazy" title='Tic-Tac-Toe' className={styles.frame} src="https://s0upernova.github.io/tic-tac-toe">
             </iframe>
           </div>
-          <h3><TypedText timeout={500} duration={500} curserStyle={"blink"} curser={`🚀`}>About this project</TypedText></h3>
+          <h3><TypedText timeout={500} duration={500} curserStyle={"blink"}>About this project</TypedText></h3>
           <p>
-            
+
             <TypedText timeout={1500} duration={2000} curserStyle={"blink"}>
               This was made using react, and started with useState for state management,
               but later converted it to useReducer
@@ -48,7 +47,7 @@ export default function Home() {
             <iframe loading="lazy" title='Raycasting' className={styles.frame} src="https://s0upernova.github.io/raycasting">
             </iframe>
           </div>
-          <h3><TypedText timeout={500} duration={500} curserStyle={"blink"} curser={`🚀`}>About this project</TypedText></h3>
+          <h3><TypedText timeout={500} duration={500} curserStyle={"blink"}>About this project</TypedText></h3>
           <p>
             <TypedText timeout={1500} duration={3500} curserStyle={"blink"}>
               This was made using typescript while following a tutorial in plain javascript,
@@ -65,10 +64,7 @@ export default function Home() {
             <TypedText timeout={8000} duration={3000} curserStyle={"blink"}>though it wasn't in the tutorial I also made a function to draw the parts of the wall between the points that hit it,
               instead of the whole wall if any hit,
               though there is the issue that it does not draw the walls between those rays if they don't hit the same wall
-
             </TypedText>
-
-
           </p>
           <p className={styles.link}>
             <i>You can check out the source code on </i>
@@ -76,6 +72,44 @@ export default function Home() {
           </p>
         </section>
 
+
+        <section className={styles.section}>
+          <p>
+            <i>This is a live preview for </i>
+            <a href="https://todo-app.me" target={'_blank'}>Todo-App</a>
+          </p>
+          <div className={styles.box}>
+            {/* // todo get cookies working in this */}
+            <iframe loading="lazy" title='Todo-App' className={styles.frame} src="https://todo-app.me">
+            </iframe>
+          </div>
+          <h3><TypedText timeout={500} duration={500} curserStyle={"blink"}>About this project</TypedText></h3>
+          <p>
+            <TypedText timeout={1500} duration={3500} curserStyle={"blink"}>
+              FYI: login does not work in the preview, because the cookie does not set.c
+              the frontend is in react, and the backend is an api made with Ruby on Rails
+            </TypedText>
+          </p>
+          <h4><TypedText timeout={5000} duration={500} curserStyle={"blink"}>Here are a few highlights</TypedText></h4>
+          <ul className={styles.list}>
+            <li><TypedText timeout={6000} duration={500} curserStyle={"blink"}>the api is on an aws ec2 instance</TypedText></li>
+            <li><TypedText timeout={7000} duration={500} curserStyle={"blink"}>the frontend is on aws amplify with ci/cd</TypedText></li>
+            <li></li>
+          </ul>
+          <p>
+            <TypedText timeout={8000} duration={4000} curserStyle={"blink"}>
+              This application presented quite a few challenges, which were a good learning experience.
+              For instance this was the first time I deployed a rails app without it being on heroku, and the saying rang pretty true,
+              that one of the hardest things to do with ruby on rails is installing it.
+            </TypedText>
+          </p>
+          <p className={styles.link}>
+            <i>You can check out the source code </i>
+            <a href="https://github.com/S0UPernova/todo_app" target={'_blank'}>Frontend</a>
+            {" "}
+            <a href="https://github.com/S0UPernova/todo_api" target={'_blank'}>Backend</a>
+          </p>
+        </section>
       </main>
     </div>
 
